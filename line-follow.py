727c003b84b462ba8a188632d208a165130ac288
Michael Ryan <michael_ryan_2000@yahoo.com>
2018-07-08 15:48:51 -0400

    Got simple line follow somewhat working

---
+++ line-follow.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from ev3dev.ev3 import *
+from time import sleep
+
+motorDriveLeft = LargeMotor("outA")
+motorDriveRight = LargeMotor("outD")
+
+sensorLight = LightSensor()
+sensorColor = ColorSensor()
+
+lcd = Screen()
+
+# Initialization
+sensorLight.mode = "REFLECT"
+sensorColor.mode = "COL-REFLECT"
+whiteIntensity = 600
+targetIntensity = 350
+blackIntensity = 280
+
+# Start Line Following
+# TODO: CHANGE FROM TRUE TO CONDITION
+leftSpeed = 300 # Speed of Left Motor
+rightSpeed = 300 # Speed of Right Motor
+speedModif = 0 # Speed Modifier for Motors
+KP = 2 # P constant for PID
+while True:
+    lcd.clear()
+    speedModif = int(KP * (sensorLight.value() - targetIntensity))
+    if speedModif < 0:
+        speedModif *= 2
+    motorDriveLeft.run_forever(speed_sp=max(200, min(900, leftSpeed + speedModif)))
+    motorDriveRight.run_forever(speed_sp=max(200, min(900, rightSpeed - speedModif)))
+    lcd.draw.text((48,13), str(sensorLight.value())
+    lcd.update()
+    sleep(0.1)
