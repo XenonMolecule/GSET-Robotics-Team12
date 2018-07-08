0299a186d5a69693e2c907d0792b00438709961b
Michael Ryan <michael_ryan_2000@yahoo.com>
2018-07-08 18:42:43 -0400

    Cleaned up code
    Also added more comments

--- line-follow.py
+++ line-follow.py
@@ -15,40 +15,42 @@ lcd = Screen()
 # Initialization
 sensorLight.mode = "REFLECT"
 sensorColor.mode = "COL-COLOR"
-whiteIntensity = 600
 targetIntensity = 375
-blackIntensity = 280
+
 
 # Start Line Following
-# TODO: CHANGE FROM TRUE TO CONDITION
 leftSpeed = 100 # Speed of Left Motor
 rightSpeed = 100 # Speed of Right Motor
 speedModif = 0 # Speed Modifier for Motors
 KP = 1.0 # P constant for PID
-lastVictimTime = 0.0
-recentVictim = False
+lastVictimTime = 0.0 # The time at which the last victim was rolled over
+recentVictim = False # Whether or not the robot detected a victim recently
 
+# TODO: CHANGE FROM TRUE TO CONDITION
 while True:
-    #lcd.clear()
+    # Use P-Loop to scale the speed modifier based on offset from optimal intensity
     speedModif = int(KP * (sensorLight.value() - targetIntensity))
     if speedModif < 0:
-        speedModif *= 6 # Increase Whippage
+        speedModif *= 6 # Increase whippage when on black lines
     else:
-        speedModif *= 1
+        speedModif *= 1 # Modify whippage on white space
+
+    # Run the motors with the modifications and within the desired speed range
     motorDriveLeft.run_forever(speed_sp=max(-100, min(900, leftSpeed + speedModif)))
     motorDriveRight.run_forever(speed_sp=max(-100, min(900, rightSpeed - speedModif)))
-    #lcd.draw.text((48,13), str(sensorLight.value())
-    #lcd.update()
 
-
-    if time() - lastVictimTime < 1:
+    # If we have detected some victims lately
+    if time() - lastVictimTime < 2:
         recentVictim = True
     else:
         recentVictim = False
 
+    # If we haven't detected any victims lately
     if not recentVictim:
+        # If the robot detects a green line (victim)
         if sensorColor.value() == 3:
             Sound.beep()
-            lastVictimTime = time()
+            lastVictimTime = time() # Reset the victim timer
 
+    # Let Eggbert sleep just a tad
     sleep(0.1)
