d9552cdd0849e6ad8553a155629a6ab4fb4ea996
Michael Ryan <michael_ryan_2000@yahoo.com>
2018-07-08 16:24:31 -0400

    tweaked values

--- line-follow.py
+++ line-follow.py
@@ -20,17 +20,19 @@ blackIntensity = 280
 
 # Start Line Following
 # TODO: CHANGE FROM TRUE TO CONDITION
-leftSpeed = 300 # Speed of Left Motor
-rightSpeed = 300 # Speed of Right Motor
+leftSpeed = 200 # Speed of Left Motor
+rightSpeed = 200 # Speed of Right Motor
 speedModif = 0 # Speed Modifier for Motors
-KP = 2 # P constant for PID
+KP = 2.5 # P constant for PID
 while True:
-    lcd.clear()
+    #lcd.clear()
     speedModif = int(KP * (sensorLight.value() - targetIntensity))
     if speedModif < 0:
-        speedModif *= 2
-    motorDriveLeft.run_forever(speed_sp=max(200, min(900, leftSpeed + speedModif)))
-    motorDriveRight.run_forever(speed_sp=max(200, min(900, rightSpeed - speedModif)))
-    lcd.draw.text((48,13), str(sensorLight.value())
-    lcd.update()
+        speedModif *= 6
+    else:
+        speedModif *= 1
+    motorDriveLeft.run_forever(speed_sp=max(100, min(900, leftSpeed - speedModif)))
+    motorDriveRight.run_forever(speed_sp=max(100, min(900, rightSpeed + speedModif)))
+    #lcd.draw.text((48,13), str(sensorLight.value())
+    #lcd.update()
     sleep(0.1)
