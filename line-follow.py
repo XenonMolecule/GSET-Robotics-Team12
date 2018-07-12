8cfd3cceaf13fe85a7229fc2152bb9821d4fcd70
Michael Ryan <michael_ryan_2000@yahoo.com>
2018-07-11 20:47:27 -0400

    Got object avoidance working

--- line-follow.py
+++ line-follow.py
@@ -9,16 +9,19 @@ motorDriveRight = LargeMotor("outD")
 
 sensorLight = LightSensor()
 sensorColor = ColorSensor()
+sensorUltraSonic = UltrasonicSensor()
 
 lcd = Screen()
 
 # Initialization
 sensorLight.mode = "REFLECT"
 sensorColor.mode = "COL-COLOR"
+sensorUltraSonic.mode = "US-DIST-IN"
 targetIntensity = 375
 
 
 # Start Line Following
+# Constants work with 7.77 Volts on Battery
 leftSpeed = 100 # Speed of Left Motor
 rightSpeed = 100 # Speed of Right Motor
 speedModif = 0 # Speed Modifier for Motors
@@ -27,6 +30,58 @@ K_Whippage_B = 0.65 # Constant for Whippage on Line
 K_Whippage_W = 1.75 # Constant for Whippage off Line
 lastVictimTime = 0.0 # The time at which the last victim was rolled over
 recentVictim = False # Whether or not the robot detected a victim recently
+
+def object_avoidance():
+    targetDistance = 45
+    speed = 300
+    time_in_range = 0
+    turn_90 = 236
+
+    # Drive up to the obstacle
+    while True:
+        speed = (sensorUltraSonic.value() - targetDistance) * 3
+        if(speed < 0):
+            motorDriveLeft.run_forever(speed_sp=max(-900, min(-50, speed)))
+            motorDriveRight.run_forever(speed_sp=max(-900, min(-50, speed)))
+        else:
+            motorDriveLeft.run_forever(speed_sp=max(50, min(900, speed)))
+            motorDriveRight.run_forever(speed_sp=max(50, min(900, speed)))
+        if(abs(speed) < 10):
+            time_in_range+=0.1
+            if(time_in_range > 1.5):
+                break
+        sleep(0.1)
+
+    motorDriveLeft.stop(stop_action="brake")
+    motorDriveRight.stop(stop_action="brake")
+
+    # Turn 90 degrees
+    motorDriveLeft.run_to_rel_pos(position_sp=-1 * turn_90, speed_sp = 200, stop_action = "hold")
+    motorDriveRight.run_to_rel_pos(position_sp=turn_90, speed_sp = 200, stop_action = "hold")
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    sleep(0.5)
+
+    # Turn in a semicircle around the obstacle
+    motorDriveLeft.run_forever(speed_sp=682)
+    motorDriveRight.run_forever(speed_sp=300)
+    sleep(1)
+
+    while sensorLight.value() > 375:
+        motorDriveLeft.run_forever(speed_sp=682)
+        motorDriveRight.run_forever(speed_sp=300)
+        sleep(0.1)
+
+    motorDriveLeft.stop(stop_action = "brake")
+    motorDriveRight.stop(stop_action = "brake")
+
+    sleep(0.5)
+
+    # Turn 90 degrees back onto the line
+    motorDriveLeft.run_to_rel_pos(position_sp = -1 * turn_90, speed_sp = 200, stop_action= "hold")
+    motorDriveRight.run_to_rel_pos(position_sp = turn_90, speed_sp = 200, stop_action = "hold")
 
 # TODO: CHANGE FROM TRUE TO CONDITION
 while True:
@@ -55,6 +110,9 @@ while True:
         if sensorColor.value() == 3:
             Sound.beep()
             lastVictimTime = time() # Reset the victim timer
+
+    if(sensorUltraSonic.value() < 50):
+        object_avoidance()
 
     # Let Eggbert sleep just a tad
     sleep(0.1)
--- object-avoidance.py
+++ object-avoidance.py
@@ -18,16 +18,21 @@ sensorLight.mode = "REFLECT"
 sensorColor.mode = "COL-COLOR"
 sensorUltraSonic.mode = "US-DIST-IN"
 
-targetDistance = 70
+targetDistance = 45
 speed = 300
 time_in_range = 0
+turn_90 = 236
 
 # Drive up to the obstacle
 while True:
-    speed = (targetDistance - sensorUltraSonic.value()) * 3
-    motorDriveLeft.run_forever(speed_sp=max(50, min(900, speed)))
-    motorDriveRight.run_forever(speed_sp=max(50, min(900, speed)))
-    if(abs(speed) < 10)
+    speed = (sensorUltraSonic.value() - targetDistance) * 3
+    if(speed < 0):
+        motorDriveLeft.run_forever(speed_sp=max(-900, min(-50, speed)))
+        motorDriveRight.run_forever(speed_sp=max(-900, min(-50, speed)))
+    else:
+        motorDriveLeft.run_forever(speed_sp=max(50, min(900, speed)))
+        motorDriveRight.run_forever(speed_sp=max(50, min(900, speed)))
+    if(abs(speed) < 10):
         time_in_range+=0.1
         if(time_in_range > 1.5):
             break
@@ -37,29 +42,29 @@ motorDriveLeft.stop(stop_action="brake")
 motorDriveRight.stop(stop_action="brake")
 
 # Turn 90 degrees
-motorDriveLeft.run_to_rel_pos(position_sp=236, speed_sp = 200, stop_action = "hold")
-motorDriveRight.run_to_rel_pos(position_sp=-236, speed_sp = 200, stop_action = "hold")
+motorDriveLeft.run_to_rel_pos(position_sp=-1 * turn_90, speed_sp = 200, stop_action = "hold")
+motorDriveRight.run_to_rel_pos(position_sp=turn_90, speed_sp = 200, stop_action = "hold")
 
 motorDriveLeft.wait_while('running')
 motorDriveRight.wait_while('running')
 
-delay(0.5)
+sleep(0.5)
 
 # Turn in a semicircle around the obstacle
-motorDriveLeft.run_forever(speed_sp=300)
-motorDriveRight.run_forever(speed_sp=900)
-delay(1)
+motorDriveLeft.run_forever(speed_sp=682)
+motorDriveRight.run_forever(speed_sp=300)
+sleep(1)
 
 while sensorLight.value() > 375:
-    motorDriveLeft.run_forever(speed_sp=300)
-    motorDriveRight.run_forever(speed_sp=900)
-    delay(0.1)
+    motorDriveLeft.run_forever(speed_sp=682)
+    motorDriveRight.run_forever(speed_sp=300)
+    sleep(0.1)
 
 motorDriveLeft.stop(stop_action = "brake")
 motorDriveRight.stop(stop_action = "brake")
 
-delay(0.5)
+sleep(0.5)
 
 # Turn 90 degrees back onto the line
-motorDriveLeft.run_to_rel_pos(position_sp = -236, speed_sp = 200, stop_action= "hold")
-motorDriveRight.run_to_rel_pos(position_sp = 236, speed_sp = 200, stop_action = "hold")
+motorDriveLeft.run_to_rel_pos(position_sp = -1 * turn_90, speed_sp = 200, stop_action= "hold")
+motorDriveRight.run_to_rel_pos(position_sp = turn_90, speed_sp = 200, stop_action = "hold")
