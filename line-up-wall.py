191de23913dcd7bd8159a1b18e4205fb42d8e5b7
Michael Ryan <michael_ryan_2000@yahoo.com>
2018-07-13 23:31:53 -0400

    Made line up work somewhat

--- line-up-wall.py
+++ line-up-wall.py
@@ -20,14 +20,18 @@ sensorUltraSonic.mode = "US-DIST-IN"
 
 def probe_direction():
     dist = sensorUltraSonic.value()
-    Sound.beep()
     # Turn Left
-    motorDriveLeft.run_to_rel_pos(position_sp=-25, speed_sp = 50, stop_action="hold")
-    motorDriveRight.run_to_rel_pos(position_sp=25, speed_sp = 50, stop_action="hold")
+    motorDriveLeft.run_to_rel_pos(position_sp=-35, speed_sp = 50, stop_action="hold")
+    motorDriveRight.run_to_rel_pos(position_sp=35, speed_sp = 50, stop_action="hold")
+    motorDriveLeft.wait_while("running")
+    motorDriveRight.wait_while("running")
+    sleep(0.1)
     new_dist = sensorUltraSonic.value()
-    motorDriveLeft.run_to_rel_pos(position_sp=25, speed_sp = 50, stop_action="hold")
-    motorDriveRight.run_to_rel_pos(position_sp=-25, speed_sp = 50, stop_action="hold")
-    if(dist < new_dist):
+    motorDriveLeft.run_to_rel_pos(position_sp=35, speed_sp = 50, stop_action="hold")
+    motorDriveRight.run_to_rel_pos(position_sp=-35, speed_sp = 50, stop_action="hold")
+    motorDriveLeft.wait_while("running")
+    motorDriveRight.wait_while("running")
+    if(new_dist < dist):
         return 0 # Target is left
     else:
         return 1 # Target is right
@@ -35,44 +39,50 @@ def probe_direction():
 # Max_turn in ticks
 def turn_to_wall(max_turn):
     last_dist = 100
-    if(probe_direction() == 0):
-        motorDriveLeft.run_to_rel_pos(position_sp=(-1 * max_turn), speed_sp = 100, stop_action="hold")
-        motorDriveRight.run_to_rel_pos(position_sp=max_turn, speed_sp = 100, stop_action="hold")
+    direction = probe_direction()
+    times_decreasing = 0
+    if(direction == 0):
+        sleep(0.2)
+        motorDriveLeft.run_to_rel_pos(position_sp=(-1 * max_turn), speed_sp = 25, stop_action="hold")
+        motorDriveRight.run_to_rel_pos(position_sp=(max_turn), speed_sp = 25, stop_action="hold")
+        sleep(1)
         # Check for wall while turning left
-        while sensorUltraSonic.value() < last_dist and (motorDriveLeft.state == "running" and motorDriveRight.state == "running"):
+        while (sensorUltraSonic.value() < last_dist) and (motorDriveLeft.state == "running" and motorDriveRight.state == "running"):
             last_dist = sensorUltraSonic.value()
             sleep(0.01)
         motorDriveLeft.stop(stop_action="brake")
         motorDriveRight.stop(stop_action="brake")
         if(sensorUltraSonic.value() < last_dist):
             sleep(0.1)
-            motorDriveLeft.run_timed(time_sp=0.01, speed_sp=100, stop_action="brake")
-            motorDriveRight.run_timed(time_sp=0.01, speed_sp=-100, stop_action="brake")
+            motorDriveLeft.run_timed(time_sp=0.01, speed_sp=50, stop_action="brake")
+            motorDriveRight.run_timed(time_sp=0.01, speed_sp=-50, stop_action="brake")
             motorDriveLeft.wait_while("running")
             motorDriveRight.wait_while("running")
             return
-        motorDriveLeft.run_to_rel_pos(position_sp=(max_turn), speed_sp = 100, stop_action="hold")
-        motorDriveRight.run_to_rel_pos(position_sp=(-1 * max_turn), speed_sp = 100, stop_action="hold")
+        motorDriveLeft.run_to_rel_pos(position_sp=(max_turn), speed_sp = 50, stop_action="hold")
+        motorDriveRight.run_to_rel_pos(position_sp=(-1 * max_turn), speed_sp = 50, stop_action="hold")
         motorDriveLeft.wait_while("running")
         motorDriveRight.wait_while("running")
     else:
-        motorDriveLeft.run_to_rel_pos(position_sp=max_turn, speed_sp = 100, stop_action="hold")
-        motorDriveRight.run_to_rel_pos(position_sp=(-1 * max_turn), speed_sp = 100, stop_action="hold")
+        sleep(0.2)
+        motorDriveLeft.run_to_rel_pos(position_sp=(max_turn), speed_sp = 50, stop_action="hold")
+        motorDriveRight.run_to_rel_pos(position_sp=(-1 * max_turn), speed_sp = 50, stop_action="hold")
+        sleep(1)
         # Check for wall while turning right
-        while sensorUltraSonic.value() < last_dist and (motorDriveLeft.state == "running" and motorDriveRight.state == "running"):
+        while (sensorUltraSonic.value() < last_dist) and (motorDriveLeft.state == "running" and motorDriveRight.state == "running"):
             last_dist = sensorUltraSonic.value()
             sleep(0.01)
         motorDriveLeft.stop(stop_action="brake")
         motorDriveRight.stop(stop_action="brake")
         if(sensorUltraSonic.value() < last_dist):
             sleep(0.1)
-            motorDriveLeft.run_timed(time_sp=0.01, speed_sp=-100, stop_action="brake")
-            motorDriveRight.run_timed(time_sp=0.01, speed_sp=100, stop_action="brake")
+            motorDriveLeft.run_timed(time_sp=0.01, speed_sp=-50, stop_action="brake")
+            motorDriveRight.run_timed(time_sp=0.01, speed_sp=50, stop_action="brake")
             motorDriveLeft.wait_while("running")
             motorDriveRight.wait_while("running")
             return
-        motorDriveLeft.run_to_rel_pos(position_sp=(-1 * max_turn), speed_sp = 100, stop_action="hold")
-        motorDriveRight.run_to_rel_pos(position_sp=(max_turn), speed_sp = 100, stop_action="hold")
+        motorDriveLeft.run_to_rel_pos(position_sp=(-1 * max_turn), speed_sp = 50, stop_action="hold")
+        motorDriveRight.run_to_rel_pos(position_sp=(max_turn), speed_sp = 50, stop_action="hold")
         motorDriveLeft.wait_while("running")
         motorDriveRight.wait_while("running")
 
