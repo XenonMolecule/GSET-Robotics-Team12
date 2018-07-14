2d97f595f181d9bc42587cabf6579f8c8e0cf6fd
Michael Ryan <michael_ryan_2000@yahoo.com>
2018-07-14 01:44:53 -0400

    Made line up not ruin already lined up positioning

--- line-up-wall.py
+++ line-up-wall.py
@@ -19,6 +19,7 @@ sensorColor.mode = "COL-COLOR"
 sensorUltraSonic.mode = "US-DIST-CM"
 
 def probe_direction():
+    turn_dir = 0
     dist = sensorUltraSonic.value()
     # Turn Left
     motorDriveLeft.run_to_rel_pos(position_sp=-35, speed_sp = 50, stop_action="hold")
@@ -33,16 +34,36 @@ def probe_direction():
     motorDriveLeft.wait_while("running")
     motorDriveRight.wait_while("running")
     if(new_dist < dist):
-        return 0 # Target is left
+        turn_dir -= 1 # Target is left
     else:
-        return 1 # Target is right
+        turn_dir += 1 # Target is right
+    dist = sensorUltraSonic.value()
+    # Turn Right
+    motorDriveLeft.run_to_rel_pos(position_sp=35, speed_sp = 50, stop_action="hold")
+    motorDriveRight.run_to_rel_pos(position_sp=-35, speed_sp = 50, stop_action="hold")
+    motorDriveLeft.wait_while("running")
+    motorDriveRight.wait_while("running")
+    sleep(0.2)
+    new_dist = sensorUltraSonic.value()
+    sleep(0.2)
+    motorDriveLeft.run_to_rel_pos(position_sp=-35, speed_sp = 50, stop_action="hold")
+    motorDriveRight.run_to_rel_pos(position_sp=35, speed_sp = 50, stop_action="hold")
+    motorDriveLeft.wait_while("running")
+    motorDriveRight.wait_while("running")
+    if(new_dist < dist):
+        turn_dir += 1 # Target is right
+    else:
+        turn_dir -= 1 # Target is left
+    return turn_dir
 
 # Max_turn in ticks
 def turn_to_wall(max_turn):
     last_dist = 1000
     direction = probe_direction()
+    print(direction)
+    print("---")
     times_increasing = 0 # times increasing in a row
-    if(direction == 0):
+    if(direction < 0):
         motorDriveLeft.run_forever(speed_sp = -75)
         motorDriveRight.run_forever(speed_sp = 75)
         sleep(0.5)
@@ -59,7 +80,7 @@ def turn_to_wall(max_turn):
             sleep(0.1)
         motorDriveLeft.stop(stop_action="hold")
         motorDriveRight.stop(stop_action="hold")
-    else:
+    elif(direction > 0):
         motorDriveLeft.run_forever(speed_sp = 75)
         motorDriveRight.run_forever(speed_sp = -75)
         sleep(0.5)
@@ -78,5 +99,3 @@ def turn_to_wall(max_turn):
         motorDriveRight.stop(stop_action="hold")
 
 turn_to_wall(500)
-
-sleep(5)
