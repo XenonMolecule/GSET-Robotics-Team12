144c2b63a485b71b798f183b684b27cb1d1d2d50
Michael Ryan <michael_ryan_2000@yahoo.com>
2018-07-15 16:58:26 -0400

    [UNTESTED] Added house to line following

--- line-follow.py
+++ line-follow.py
@@ -165,6 +165,149 @@ def turn_to_wall(max_turn):
         motorDriveLeft.stop(stop_action="hold")
         motorDriveRight.stop(stop_action="hold")
 
+def drive_to_dist(target_dist_mm):
+    targetDistance = 35
+    speed = 300
+    time_in_range = 0
+
+    # Drive up to the obstacle
+    while True:
+        speed = (sensorUltraSonic.value() - target_dist_mm) * 2.5
+        if(speed < 0):
+            motorDriveLeft.run_forever(speed_sp=max(-900, min(-50, speed)))
+            motorDriveRight.run_forever(speed_sp=max(-900, min(-50, speed)))
+        else:
+            motorDriveLeft.run_forever(speed_sp=max(50, min(900, speed)))
+            motorDriveRight.run_forever(speed_sp=max(50, min(900, speed)))
+        if(abs(speed) < 10):
+            time_in_range+=0.1
+            if(time_in_range > 1.5):
+                break
+        sleep(0.1)
+    motorDriveLeft.stop(stop_action = "hold")
+    motorDriveRight.stop(stop_action = "hold")
+
+
+def look_for_ball():
+    drive_to_dist(40)
+
+    motorDriveLeft.run_timed(time_sp=500, speed_sp = 100)
+    motorDriveRight.run_timed(time_sp=500, speed_sp = 100)
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    motorDriveLeft.run_to_rel_pos(position_sp = -100, speed_sp = 100)
+    motorDriveRight.run_to_rel_pos(position_sp = -100, speed_sp = 100)
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    motorDriveLeft.run_to_rel_pos(position_sp = -236, speed_sp = 150, stop_action = "hold")
+    motorDriveRight.run_to_rel_pos(position_sp = 236, speed_sp = 150, stop_action = "hold")
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    drive_to_dist(40)
+
+    motorDriveLeft.run_timed(time_sp=500, speed_sp = 100)
+    motorDriveRight.run_timed(time_sp=500, speed_sp = 100)
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    motorDriveLeft.run_to_rel_pos(position_sp = -100, speed_sp = 100)
+    motorDriveRight.run_to_rel_pos(position_sp = -100, speed_sp = 100)
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    motorDriveLeft.run_to_rel_pos(position_sp = 225, speed_sp = 150, stop_action = "hold")
+    motorDriveRight.run_to_rel_pos(position_sp = -225, speed_sp = 150, stop_action = "hold")
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    # LOOK FOR IR BALL
+    if(sensorIR.value()!=0):
+        return
+    sleep(1)
+
+    motorDriveLeft.run_to_rel_pos(position_sp = 50, speed_sp = 150, stop_action = "hold")
+    motorDriveRight.run_to_rel_pos(position_sp = 50, speed_sp = 150, stop_action = "hold")
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    motorDriveLeft.run_to_rel_pos(position_sp = 225, speed_sp = 150, stop_action = "hold")
+    motorDriveRight.run_to_rel_pos(position_sp = -225, speed_sp = 150, stop_action = "hold")
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    drive_to_dist(40)
+
+    motorDriveLeft.run_to_rel_pos(position_sp = -100, speed_sp = 150, stop_action = "hold")
+    motorDriveRight.run_to_rel_pos(position_sp = -100, speed_sp = 150, stop_action = "hold")
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    motorDriveLeft.run_to_rel_pos(position_sp = -236, speed_sp = 150, stop_action = "hold")
+    motorDriveRight.run_to_rel_pos(position_sp = 236, speed_sp = 150, stop_action = "hold")
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    # LOOK FOR IR BALL
+    if(sensorIR.value()!=0):
+        return
+    sleep(1)
+
+    motorDriveLeft.run_to_rel_pos(position_sp = -100, speed_sp = 150, stop_action = "hold")
+    motorDriveRight.run_to_rel_pos(position_sp = -100, speed_sp = 150, stop_action = "hold")
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    motorDriveLeft.run_to_rel_pos(position_sp = -230, speed_sp = 150, stop_action = "hold")
+    motorDriveRight.run_to_rel_pos(position_sp = 230, speed_sp = 150, stop_action = "hold")
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    drive_to_dist(300)
+
+    motorDriveLeft.run_to_rel_pos(position_sp = 230, speed_sp = 150, stop_action = "hold")
+    motorDriveRight.run_to_rel_pos(position_sp = -230, speed_sp = 150, stop_action = "hold")
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    motorDriveLeft.run_to_rel_pos(position_sp = 300, speed_sp = 150, stop_action = "hold")
+    motorDriveRight.run_to_rel_pos(position_sp = 300, speed_sp = 150, stop_action = "hold")
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    motorDriveLeft.run_to_rel_pos(position_sp = 115, speed_sp = 150, stop_action = "hold")
+    motorDriveRight.run_to_rel_pos(position_sp = -115, speed_sp = 150, stop_action = "hold")
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    motorDriveLeft.run_to_rel_pos(position_sp = 300, speed_sp = 150, stop_action = "hold")
+    motorDriveRight.run_to_rel_pos(position_sp = 300, speed_sp = 150, stop_action = "hold")
+
+    motorDriveLeft.wait_while('running')
+    motorDriveRight.wait_while('running')
+
+    # LOOK FOR IR BALL
+    if(sensorIR.value()!=0):
+        return
+    sleep(1)
+
 while sensorColor.value() != 5:
     # Use P-Loop to scale the speed modifier based on offset from optimal intensity
     speedModif = int(KP * (sensorLight.value() - targetIntensity))
@@ -208,3 +351,13 @@ sensorUltraSonic.mode = "US-DIST-CM"
 sleep(1)
 
 turn_to_wall(200)
+
+look_for_ball()
+
+motorDriveLeft.run_to_rel_pos(position_sp = 400, speed_sp = 150, stop_action = "hold")
+motorDriveRight.run_to_rel_pos(position_sp = 400, speed_sp = 150, stop_action = "hold")
+
+motorDriveLeft.wait_while('running')
+motorDriveRight.wait_while('running')
+
+Sound.beep()
